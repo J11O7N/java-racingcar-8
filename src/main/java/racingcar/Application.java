51c193bdac2880d63d2d48650fee2f46ac8b122b
Joon <wjdwns011007@naver.com>
2025-10-27 03:23:41 +0900

    feat(racingcar): Application 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+import racingcar.domain.generator.NumberGenerator;
+import racingcar.domain.generator.RandomNumberGenerator;
+import racingcar.domain.rule.MoveRule;
+import racingcar.domain.rule.ThresholdMoveRule;
+import racingcar.service.RaceService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MoveRule rule = new ThresholdMoveRule();
+        NumberGenerator generator = new RandomNumberGenerator();
+
+        RaceService service = new RaceService(generator, rule);
+        RaceController controller = new RaceController(service);
+        controller.run();
     }
 }
