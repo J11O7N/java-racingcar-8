db1c628b9a96876d5b99ea470e8fbac7b76ed085
Joon <wjdwns011007@naver.com>
2025-10-27 03:20:30 +0900

    feat(controller): RaceController 구현 (전체 흐름 제어)

---
+++ src/main/java/racingcar/controller/RaceController.java
@@ -0,0 +1,28 @@
+package racingcar.controller;
+
+import racingcar.domain.race.RaceRecord;
+import racingcar.service.RaceService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class RaceController {
+    private final RaceService raceService; // DI
+
+    public RaceController(RaceService raceService) {
+        this.raceService = raceService;
+    }
+
+    public void run() {
+        List<String> names = InputView.readCarNames();     // 입력
+        int attempts = InputView.readAttemptCount();
+
+        raceService.registerCars(names);                        // Cars 준비
+        RaceRecord record = raceService.play(attempts);    // 경기 진행
+
+        OutputView.printStart();
+        OutputView.printRaceResult(record);
+        OutputView.printWinners(raceService.getFindWinners()); // 공동 우승 처리
+    }
+}
